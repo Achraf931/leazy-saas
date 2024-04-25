92ed9f3f97451e2e2bd717d72c6cac90dd87556d
Charfeddine Hamrouni <c.hamrouni@useradgents.com>
2024-04-25 23:46:37 +0200

    fix: change middleware auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ export default defineNuxtConfig({
       onGuestOnly: '/',
     },
     globalMiddleware: {
-      enabled: false,
+      enabled: true,
       allow404WithoutAuth: true,
     },
   },
