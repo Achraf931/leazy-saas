f192d8b001f270372af9c63df1843dbfb40fe065
Charfeddine Hamrouni <45672435+Achraf931@users.noreply.github.com>
2024-05-27 19:45:42 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,7 +47,7 @@ export default defineNuxtConfig({
       onGuestOnly: '/',
     },
     globalMiddleware: {
-      enabled: true,
+      enabled: false,
       allow404WithoutAuth: true,
     },
   },
